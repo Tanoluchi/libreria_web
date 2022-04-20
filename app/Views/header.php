6e31bbe95e010380bccae0ab820fb7359b159408
Tanoluchi <ejemplolucianox15867@gmail.com>
2022-04-20 14:49:12 -0300

    cambio de nombre de archivos

--- app/Views/header.php
+++ app/Views/header.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="assets/css/style.css">
     <link rel="stylesheet" type="text/css" href="assets/css/font-awesome.min.css">
-    <title>Barra de Navegacion</title>
+    <title>Cabecera</title>
 </head>
 <body>
     <header class="header">
