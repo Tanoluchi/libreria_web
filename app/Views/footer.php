8c336fd2a915bf94c0eb5a8adc1e5681d732cf1e
Tanoluchi <ejemplolucianox15867@gmail.com>
2022-04-20 14:46:18 -0300

    first-commit

---
+++ app/Views/footer.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    <title>Footer</title>
+</head>
+<body>
+    <footer class="pie-pagina">
+        <div class="grupo-1">
+            <div class="box">
+                <figure>
+                    <a href="#"><img src="assets/img/logo.png" alt="LOGO MUNDO LIBRO (LIBRO ABIERTO)"></a>
+                </figure>
+            </div>
+            <div class="box">
+                <h2>SOBRE NOSOTROS</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum ullam accusamus distinctio quo impedit. Eius architecto maxime sed dolore vero? Blanditiis libero magnam aliquid. Aperiam magni accusamus quae ipsum repellat.</p>
+            </div>
+            <div class="box">
+                <h2>SIGUENOS</h2>
+                <div class="red-social">
+                    <a href="#"><img src="assets/img/facebook.svg" alt=""></a>
+                    <a href="#"><img src="assets/img/instagram.svg" alt=""></a>
+                    <a href="#"><img src="assets/img/twitter.svg" alt=""></a>
+                </div>
+            </div>
+        </div>
+        <div class="grupo-2">
+            <small>&copy; 2021 <b>MUNDO LIBRO</b> - Todos los derechos reservados</small>
+        </div>
+    </footer>
+</body>
+</html>
